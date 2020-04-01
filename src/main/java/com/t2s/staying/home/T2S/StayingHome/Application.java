d863fe6e3e91e4a2d953090778913fcbc0672a13
Angjelo Todhri <a.todhri@intralot.com>
2020-04-02 01:23:01 +0300

    Initial commit

---
+++ src/main/java/com/t2s/staying/home/T2S/StayingHome/Application.java
@@ -0,0 +1,13 @@
+package com.t2s.staying.home.T2S.StayingHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
