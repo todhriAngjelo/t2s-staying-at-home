b57937512cbd9cc6d30acb0c94a3ec4d97555099
Angjelo Todhri <a.todhri@intralot.com>
2020-05-03 01:09:05 +0300

    track creation time and also store author at file's metadata

--- src/main/java/com/t2s/staying/home/T2S/StayingHome/ApplicationConstants.java
+++ src/main/java/com/t2s/staying/home/T2S/StayingHome/ApplicationConstants.java
@@ -10,4 +10,7 @@ public class ApplicationConstants {
 	// commands factory string values
 	public static final String NEW_DOCUMENT_COMMAND = "newDocument";
 
+	// todo this can go in a configuration file
+	public static final String DEFAULT_SAVE_FILE_LOCATION = "C:\\Users\\a.todhri\\Documents\\T2Sdocs\\";
+
 }
--- src/main/java/com/t2s/staying/home/T2S/StayingHome/command/NewDocument.java
+++ src/main/java/com/t2s/staying/home/T2S/StayingHome/command/NewDocument.java
@@ -1,9 +1,18 @@
 package com.t2s.staying.home.T2S.StayingHome.command;
 
+import static com.t2s.staying.home.T2S.StayingHome.ApplicationConstants.DEFAULT_SAVE_FILE_LOCATION;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.DosFileAttributes;
+import java.nio.file.attribute.UserDefinedFileAttributeView;
 
 import com.t2s.staying.home.T2S.StayingHome.model.Document;
 
@@ -14,12 +23,35 @@ public class NewDocument implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		try {
-			File myObj = new File(String.format("%s.txt", document.getTitle() )); // todo handle document null
-			// todo save this in default location defined in app constants
+			if (document.getTitle() == null) {
+				System.out.println("Can not create a document with an empty title.");
+			}
+			File myObj = new File(String.format(DEFAULT_SAVE_FILE_LOCATION + "%s.txt", document.getTitle()));
+
+			String path = DEFAULT_SAVE_FILE_LOCATION + document.getTitle() + ".txt";
+			Path file = Paths.get(path);
+
 			if (myObj.createNewFile()) {
 				System.out.println("File created: " + myObj.getName()); // todo implement ui success message
+
+				// write metadata
+				UserDefinedFileAttributeView view =
+						Files.getFileAttributeView(Paths.get(path), UserDefinedFileAttributeView.class);
+				view.write("user.author",
+						Charset.defaultCharset().encode(document.getAuthorsName()));
+
 			} else {
 				System.out.println("File already exists."); // todo implement ui . already exists file
+
+				// read metadata
+				UserDefinedFileAttributeView view =
+						Files.getFileAttributeView(Paths.get(path), UserDefinedFileAttributeView.class);
+				ByteBuffer buf = ByteBuffer.allocate(view.size("user.author"));
+				view.read("user.author", buf);
+				buf.flip();
+				String value = Charset.defaultCharset().decode(buf).toString();
+				System.out.println("author is:" + value);
+
 			}
 		} catch (IOException e) {
 			System.out.println("An error occurred.");
