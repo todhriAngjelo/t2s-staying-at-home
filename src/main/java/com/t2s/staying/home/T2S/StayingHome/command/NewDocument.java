180096a0bbe9863fab0e8baec73c38f23d5a664f
Angjelo Todhri <a.todhri@intralot.com>
2020-05-03 15:54:53 +0300

    refactor metadata into utility class

--- src/main/java/com/t2s/staying/home/T2S/StayingHome/command/NewDocument.java
+++ src/main/java/com/t2s/staying/home/T2S/StayingHome/command/NewDocument.java
@@ -9,12 +9,11 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.DosFileAttributes;
 import java.nio.file.attribute.UserDefinedFileAttributeView;
 
 import com.t2s.staying.home.T2S.StayingHome.model.Document;
+import com.t2s.staying.home.T2S.utils.FileUtils;
 
 public class NewDocument implements ActionListener {
 
@@ -29,35 +28,18 @@ public class NewDocument implements ActionListener {
 			File myObj = new File(String.format(DEFAULT_SAVE_FILE_LOCATION + "%s.txt", document.getTitle()));
 
 			String path = DEFAULT_SAVE_FILE_LOCATION + document.getTitle() + ".txt";
-			Path file = Paths.get(path);
 
 			if (myObj.createNewFile()) {
 				System.out.println("File created: " + myObj.getName()); // todo implement ui success message
-
-				// write metadata
-				UserDefinedFileAttributeView view =
-						Files.getFileAttributeView(Paths.get(path), UserDefinedFileAttributeView.class);
-				view.write("user.author",
-						Charset.defaultCharset().encode(document.getAuthorsName()));
-
+				FileUtils.setFileMetadata(path, "author", document.getAuthorsName());
 			} else {
 				System.out.println("File already exists."); // todo implement ui . already exists file
-
-				// read metadata
-				UserDefinedFileAttributeView view =
-						Files.getFileAttributeView(Paths.get(path), UserDefinedFileAttributeView.class);
-				ByteBuffer buf = ByteBuffer.allocate(view.size("user.author"));
-				view.read("user.author", buf);
-				buf.flip();
-				String value = Charset.defaultCharset().decode(buf).toString();
-				System.out.println("author is:" + value);
-
+				FileUtils.getFileMetadata(path, "author");
 			}
 		} catch (IOException e) {
 			System.out.println("An error occurred.");
 			e.printStackTrace();
 		}
-
 	}
 
 	public void setDocument(Document document) {
