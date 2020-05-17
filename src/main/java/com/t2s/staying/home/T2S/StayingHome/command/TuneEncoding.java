bb3ec1f5e16d14d91769df341ade0127f6dfe3f1
Angjelo Todhri <a.todhri@intralot.com>
2020-05-17 17:44:54 +0300

    fix

--- src/main/java/com/t2s/staying/home/T2S/StayingHome/command/TuneEncoding.java
+++ src/main/java/com/t2s/staying/home/T2S/StayingHome/command/TuneEncoding.java
@@ -3,11 +3,20 @@ package com.t2s.staying.home.T2S.StayingHome.command;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import com.t2s.staying.home.T2S.StayingHome.view.DocumentEditorView;
+
 public class TuneEncoding implements ActionListener {
+
+	DocumentEditorView view;
+
+	TuneEncoding(DocumentEditorView view) {
+		this.view = view;
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
+//		EncodingUtils.encode(String);
 		// todo
 	}
 }
